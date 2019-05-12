549f6f22f12b761a71f62eeab98718db0b7710c0
igor.kpl <lov3catch@gmail.com>
2019-05-12 20:22:14 +0300

    add controller

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+// src/Controller/DefaultController.php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index(): Response
+    {
+        return new Response('It works! ☺');
+    }
+}
