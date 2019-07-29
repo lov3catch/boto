95bcbc12cbffc59e25c71aedb7d694cf06980c00
igor.kpl <lov3catch@gmail.com>
2019-07-29 20:43:19 +0300

    hz mojet zarabotayet

--- src/Botonarioum/Bots/Handlers/Pipes/MusicDealer/Keyboards/TrackFinderSearchResponseKeyboard.php
+++ src/Botonarioum/Bots/Handlers/Pipes/MusicDealer/Keyboards/TrackFinderSearchResponseKeyboard.php
@@ -23,11 +23,11 @@ class TrackFinderSearchResponseKeyboard
 
     private function attachContentPart(array &$keyboard, TrackFinderSearchResponse $response, Update $update): void
     {
-        $keyboard = array_map(function (array $item) {
+        $keyboard[] = array_map(function (array $item) {
             $title = $item[0];
             // todo: Реализовать класс для работы с провайдерами
             $callbackData = implode('::', ['zn', $item[1]]);
-            return [InlineKeyboardButton::withCallbackData($title, $callbackData)];
+            return InlineKeyboardButton::withCallbackData($title, $callbackData);
         }, $response->getData());
     }
 
