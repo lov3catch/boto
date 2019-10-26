f04d1edaed536d44ae179b18e3a83ee44346dda5
igor.kpl <lov3catch@gmail.com>
2019-10-26 12:17:05 +0300

    add url

--- src/Botonarioum/Bots/Handlers/Pipes/MusicDealer/DownloadCallbackPipe.php
+++ src/Botonarioum/Bots/Handlers/Pipes/MusicDealer/DownloadCallbackPipe.php
@@ -21,6 +21,7 @@ class DownloadCallbackPipe extends CallbackPipe
     {
         $this->sendAnswer($bot, $update);
         $downloadUrl = $this->buildDownloadUrl($bot, $update);
+        var_dump($downloadUrl);
         $content = Requests::get($downloadUrl, [], ['proxy' => 'http://FqX3tB:46VjdC@91.243.52.252:8000']);
         $inputFile = new InputFile('sound.mp3', $content->body);
         $bot->sendDocument(SendDocument::withInputFile($update->getCallbackQuery()->getMessage()->getChat()->getId(), $inputFile));
