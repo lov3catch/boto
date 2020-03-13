c465c07128d54651a76c899b70e6982a062b97d1
lov3catch <lov3catch@gmail.com>
2020-03-13 10:37:11 +0200

    fix

--- src/Botonarioum/Bots/Handlers/ModeratorHandler.php
+++ src/Botonarioum/Bots/Handlers/ModeratorHandler.php
@@ -251,7 +251,7 @@ class ModeratorHandler extends AbstractHandler
     private function init(): void
     {
         $this
-//            ->add($this->allSupportPipe)
+            ->add($this->allSupportPipe)
             ->add($this->startPipe)             // message
             ->add($this->disableLinkPipe)
             ->add($this->enableLinkPipe)
