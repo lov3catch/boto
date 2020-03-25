bb3d4dd94740f3c9d481e0c7af924df2e73f32a3
Ihor Kopyl <lov3catch@gmail.com>
2020-03-25 22:14:33 +0200

    remove keyboard if admin send /start

--- src/Botonarioum/Bots/Handlers/Pipes/Moderator/StartPipe.php
+++ src/Botonarioum/Bots/Handlers/Pipes/Moderator/StartPipe.php
@@ -4,12 +4,14 @@ namespace App\Botonarioum\Bots\Handlers\Pipes\Moderator;
 
 use App\Botonarioum\Bots\Handlers\Pipes\AbstractPipe;
 use App\Botonarioum\Bots\Helpers\GetMe;
+use App\Botonarioum\Bots\Helpers\IsChatAdministrator;
 use App\Entity\ModeratorStart;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Formapro\TelegramBot\Bot;
 use Formapro\TelegramBot\KeyboardButton;
 use Formapro\TelegramBot\ReplyKeyboardMarkup;
+use Formapro\TelegramBot\ReplyKeyboardRemove;
 use Formapro\TelegramBot\SendMessage;
 use Formapro\TelegramBot\Update;
 
@@ -30,6 +32,31 @@ class StartPipe extends AbstractPipe
 
     public function processing(Bot $bot, Update $update): bool
     {
+        // Если это в группе - то...
+        if ($update->getMessage()->getChat()->getId() < 0) {
+            $isUserAdmin = (new IsChatAdministrator($bot, $update->getMessage()->getChat()))->checkUser($update->getMessage()->getFrom());
+
+            // Если /start пытается вызвать НЕ админ - игнорим
+            if (false === $isUserAdmin) {
+                return true;
+            }
+
+            // Если /start вызывает админ - удаляем клавиатуру (т.к. многие повызывали клавиатуру в чатах)
+            if (true === $isUserAdmin) {
+
+                $message = (new SendMessage(
+                    $update->getMessage()->getChat()->getId(),
+                    'Настройки бота лучше производить в личном чате с ботом :)'
+                ));
+
+                $message->setReplyMarkup(new ReplyKeyboardRemove());
+
+                $bot->sendMessage($message);
+
+                return true;
+            }
+        }
+
         $text = 'Привет! Это бот-модератор для твоих групп. Добавь его в свою группу, дай права админа (не волнуйся, бот не сделает ничего плохого). Затем настрой его по своему вкусу, либо используй готовые настройки и пользуйся.';
         $message = new SendMessage(
             $update->getMessage()->getChat()->getId(),
